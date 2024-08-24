523d98b559bdb4aa8a2014182e22f5708d38efbd
Jayjeet1013 <jayjeet1013@gmail.com>
2024-08-24 15:41:34 +0530

    add api route and home page

---
+++ app/api/chat/route.js
@@ -0,0 +1,70 @@
+import { NextResponse } from "next/server";
+import { Pinecone } from "@pinecone-database/pinecone";
+import OpenAI from "openai";
+
+const systemPrompt = `
+You are a Rate My Professor agent designed to assist students in finding the best classes by answering their questions. For each user query, analyze the question to understand the specific needs, such as preferred teaching styles, course difficulty, or professor ratings. Return the top 5 professors who best match the criteria based on student reviews, course content, and teaching effectiveness. Use the information about these professors to provide a detailed and helpful response to the user’s question. Always prioritize accurate and relevant information to ensure students make informed decisions about their class selections.
+`;
+
+export async function POST(req) {
+  const data = await req.json();
+  const pc = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY,
+  });
+  const index = pc.index("rag").namespace("ns1");
+  const openai = new OpenAI();
+  const text = data[data.length - 1].content;
+  const embedding = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input: text,
+    encoding_format: "float",
+  });
+  const results = await index.query({
+    topK: 5,
+    includeMetadata: true,
+    vector: embedding.data[0].embedding,
+  });
+  let resultString = "\n\n Returned results from vector db done automatically";
+  results.matches.forEach((match) => {
+    resultString += `
+        Returned Results:
+        Professor: ${match.id}
+        Review: ${match.metadata.stars}
+        Subject: ${match.metadata.subject}
+        Stars: ${match.metadata.stars}
+        \n\n`;
+  });
+  const lastMessage = data[data.length - 1];
+  const lastMessageContent = lastMessage.content + resultString;
+  const lastDataWithoutLastMessage = data.slice(0, data.length - 1);
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: systemPrompt },
+      ...lastDataWithoutLastMessage,
+      { role: "user", content: lastMessageContent },
+    ],
+    model: "gpt-3.5-turbo",
+    stream: true,
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+      try {
+        for await (const chunk of completion) {
+          const content = chunk.choices[0]?.delta?.content;
+          if (content) {
+            const text = encoder.encode(content);
+            controller.enqueue(text);
+          }
+        }
+      } catch (err) {
+        controller.error(err);
+      } finally {
+        controller.close();
+      }
+    },
+  });
+  return new NextResponse(stream);
+}
